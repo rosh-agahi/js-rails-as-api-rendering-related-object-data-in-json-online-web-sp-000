afeba32c7db584423e5f9341182efe4e9e866c29
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-14 03:44:41 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,9 @@
 class SightingsController < ApplicationController
   
-  
+  def index
+    sightings = Sighting.all
+    render json: sightings.to_json(include: [:bird, :location])
+  end
   
   def show
     sighting = Sighting.find_by(id: params[:id])
